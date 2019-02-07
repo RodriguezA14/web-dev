4c6f43e52679cd56a5e174c3bc6462365d637839
Marcus <student.150918@worc.k12.ma.us>
2019-02-07 16:24:02 +0000

    update

---
+++ projects/ex49_test/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'My Project',
+    'author': 'Marcus',
+    'url': 'https://ide.c9.io/marcusaj0114/web-dev',
+    'download_url': 'https://ide.c9.io/marcusaj0114/web-dev',
+    'author_email': 'student.150918@worcesterschools.net',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['ex49'],
+    'scripts': [],
+    'name': 'myenv'
+}
+
+setup(**config)
